02f8f2aff93de8fa1b6277e0c55207644b0af912
tanelmetsar <tanel.metsar@tktk.ee>
2024-04-07 19:06:59 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //output
+//string - söne
+string userName = Console.ReadLine(); //input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !"); //output
